db47fb1ff4fffd66c34d997b611f220219d751f6
crapthings <crapthings@gmail.com>
2016-05-05 12:35:04 +0800

    knock knock

--- client/main.js
+++ client/main.js
@@ -6,20 +6,50 @@ import faker from 'faker'
 
 import d3 from 'd3'
 
-Template.container.onRendered(function () {
+Template.container.onRendered(function() {
 
-	let data = [15, 300, 167, 7, 9]
+	let data = _.times(30, function() {
+		return {
+			start: faker.date.past(_.random(30))
+		}
+	})
+
+	let data2 = _.times(10, function() {
+		return {
+			start: faker.date.past(_.random(30))
+		}
+	})
+
+	let data3 = _.times(10, function() {
+		return {
+			start: faker.date.past(_.random(30))
+		}
+	})
+
+	let data5 = _.times(10, function() {
+		return {
+			start: faker.date.past(_.random(30)),
+			value: _.random(700)
+		}
+	})
+
+	let minDate = d3.min(data, function(d) {
+		return d.start
+	})
+
+	let maxDate = d3.max(data, function(d) {
+		return d.start
+	})
 
 	let $container = d3.select(this.find('.container'))
 
 	let xScale = d3.time.scale()
-		.domain([new Date(1980, 1, 1), new Date(2017, 1, 1)])
+		.domain([minDate, maxDate])
 		.range([40, 600])
 
 	let xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient('bottom')
-		.ticks(d3.time.day, 1)
 
 	let zoom = d3.behavior.zoom()
 		.x(xScale)
@@ -38,9 +68,89 @@ Template.container.onRendered(function () {
 		.attr('transform', 'translate(0, 0)')
 		.call(xAxis)
 
+	let $g1 = $svg
+		.append('g')
+		.attr('width', 640)
+		.attr('height', 480)
+
+	let $line = $g1
+		.append('line')
+		.attr('x1', 0)
+		.attr('x2', 640)
+		.style('stroke', 'red')
+		.style('stroke-width', 10)
+		.style('transform', 'translate(0, 50px)')
+
+	let $circles = $g1.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('class', 'circle')
+		.attr('cx', function(d) {
+			return xScale(d.start)
+		})
+		.attr('cy', 50)
+		.attr('r', 5)
+
+	let $g2 = $svg
+		.append('g')
+		.attr('width', 640)
+		.attr('height', 480)
+
+	let $circles2 = $g2.selectAll('circle')
+		.data(data2)
+		.enter()
+		.append('circle')
+		.attr('class', 'circle')
+		.attr('cx', function(d) {
+			return xScale(d.start)
+		})
+		.attr('cy', 100)
+		.attr('r', 5)
+
+	let $g3 = $svg
+		.append('g')
+		.attr('width', 640)
+		.attr('height', 480)
+
+	let $circles3 = $g3.selectAll('circle')
+		.data(data3)
+		.enter()
+		.append('circle')
+		.attr('class', 'circle')
+		.attr('cx', function(d) {
+			return xScale(d.start)
+		})
+		.attr('cy', 150)
+		.attr('r', 5)
+
+	let $g5 = $svg
+		.append('g')
+		.attr('width', 640)
+		.attr('height', 480)
+
+	let $bars = $g3.selectAll('rect')
+		.data(data5)
+		.enter()
+		.append('rect')
+		.attr('class', 'bar')
+		.attr('width', 10)
+		.attr('height', function (d) {
+			return (d.value)
+		})
+		.attr('x', function(d) {
+			return xScale(d.start)
+		})
+		.attr('y', 200)
+
 	function zoomed(a, b, c) {
-		console.log(a, b, c)
 		$svg.select(".x.axis").call(xAxis)
+		$svg.selectAll("circle.circle").attr('cx', function(d) {
+			return xScale(d.start)
+		})
+		$svg.selectAll("rect.bar").attr('x', function(d) {
+			return xScale(d.start)
+		})
 	}
 
 })
